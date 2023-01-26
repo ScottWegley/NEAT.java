fbfbcf4b96515ba24565528f3a0916884a79fa51
scottwegley <scottwegley@gmail.com>
2023-01-26 00:49:57 -0800

    started work on network object

---
+++ src/Network.java
@@ -0,0 +1,51 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import src.NodeGene.TYPE;
+
+public class Network {
+    
+    public static final int MAX_NODES = (int)Math.pow(2,10);
+
+    private HashMap<ConnectionGene, ConnectionGene> connectionDictionary  = new HashMap<>();
+    private ArrayList<NodeGene> nodeDictionary = new ArrayList<>();
+
+    private int inputNodes, outputNodes, instanceLimit, instances;
+
+    public Network(int inputNodes, int outputNodes, int instanceLimit){
+        this.reset(inputNodes, outputNodes, instanceLimit);
+    }
+
+    public void reset(int inputNodes, int outputNodes, int instanceLimit){
+        this.inputNodes = inputNodes;
+        this.outputNodes = outputNodes;
+        this.instanceLimit = instanceLimit;
+
+        connectionDictionary.clear();
+        nodeDictionary.clear();
+
+        for (int i = 0; i < inputNodes; i++) {
+            NodeGene nodeGene = generateNode();
+            nodeGene.setType(TYPE.INPUT_NODE);
+        }
+
+        for (int i = 0; i < outputNodes; i++) {
+            NodeGene nodeGene = generateNode();
+            nodeGene.setType(TYPE.OUTPUT_NODE);
+        }
+    }
+
+    public NodeGene generateNode() {
+        NodeGene nodeGene = new NodeGene(nodeDictionary.size() + 1);
+        nodeDictionary.add(nodeGene);
+        return nodeGene;
+    }
+
+    public NodeGene getNode(int index) {
+        if(index >= 0 && index < nodeDictionary.size()) return nodeDictionary.get(index);
+        return generateNode();
+    }
+
+}
