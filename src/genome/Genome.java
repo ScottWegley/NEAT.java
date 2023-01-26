393be5ee37449e66900226b301a0d55831cc590f
scottwegley <scottwegley@gmail.com>
2023-01-26 10:24:57 -0800

    basic genome functions

--- src/genome/Genome.java
+++ src/genome/Genome.java
@@ -1,5 +1,39 @@
 package src.genome;
 
+import src.network.Network;
+import src.utils.CustomHashSet;
+
 public class Genome {
-    
+    private CustomHashSet<ConnectionGene> connections = new CustomHashSet<>();
+    private CustomHashSet<NodeGene> nodes = new CustomHashSet<>();
+
+    private Network network;
+
+    public Genome(Network network) {
+        this.network = network;
+    }
+
+    public static double distance(Genome g1, Genome g2){
+        return 0;
+    }
+
+    public static Genome crossOver(Genome g1, Genome g2){
+        return null;
+    }
+
+    public void mutate(){
+
+    }
+
+    public CustomHashSet<ConnectionGene> getConnections() {
+        return connections;
+    }
+
+    public CustomHashSet<NodeGene> getNodes() {
+        return nodes;
+    }
+
+    public Network getNetwork() {
+        return network;
+    }
 }
