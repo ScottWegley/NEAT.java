b72a6fec20d5dee03d7d9a216d35496e07f509ee
scottwegley <scottwegley@gmail.com>
2023-01-26 10:25:11 -0800

    basic connection gene

--- src/genome/ConnectionGene.java
+++ src/genome/ConnectionGene.java
@@ -1,5 +1,56 @@
 package src.genome;
 
+import src.network.Network;
+
 public class ConnectionGene {
-    
+    private NodeGene from, to;
+
+    private double weight;
+    private boolean expressed = true;
+
+    public ConnectionGene(NodeGene from, NodeGene to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public NodeGene getFrom() {
+        return from;
+    }
+
+    public void setFrom(NodeGene from) {
+        this.from = from;
+    }
+
+    public NodeGene getTo() {
+        return to;
+    }
+
+    public void setTo(NodeGene to) {
+        this.to = to;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public boolean isExpressed() {
+        return expressed;
+    }
+
+    public void setExpressed(boolean expressed) {
+        this.expressed = expressed;
+    }
+
+    public boolean equals(Object o){
+        if(!(o instanceof ConnectionGene)) return false;
+        return (from.equals(((ConnectionGene)o).getFrom()) && to.equals(((ConnectionGene)o).getTo()));
+    }
+
+    public int hashCode() {
+        return from.getInnovationNumber() * Network.MAX_NODES + to.getInnovationNumber();
+    }
 }
