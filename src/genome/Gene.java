c405596ebdc701558b289af8af783826af8b3d00
scottwegley <scottwegley@gmail.com>
2023-01-26 10:09:12 -0800

    gene to derive node and connection from

--- src/genome/Gene.java
+++ src/genome/Gene.java
@@ -1,5 +1,16 @@
 package src.genome;
-
 public class Gene {
-    
+    protected int innovationNumber;
+
+    public Gene(int innovationNumber) {
+        this.innovationNumber = innovationNumber;
+    }
+
+    public int getInnovationNumber() {
+        return innovationNumber;
+    }
+
+    public void setInnovationNumber(int innovationNumber) {
+        this.innovationNumber = innovationNumber;
+    }
 }
