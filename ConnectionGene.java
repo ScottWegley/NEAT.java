f32aef351eb5f5e9130302a5f0a7adb10e3abd41
scottwegley <scottwegley@gmail.com>
2023-01-25 18:32:38 -0800

    Basic Gene Classes

---
+++ ConnectionGene.java
@@ -0,0 +1,8 @@
+public class ConnectionGene {
+    
+    private int innovationNumber;
+    private boolean expressed;
+    private float weight;
+    private int inputNodeID, outputNodeID;
+
+}
---
+++ NodeGene.java
@@ -0,0 +1,11 @@
+public class NodeGene {
+
+    private int id;
+    private TYPE type;
+
+    public enum TYPE {
+        INPUT_NODE,
+        HIDDEN_NODE,
+        OUTPUT_NODE
+    }
+}
