c86c10f9a2446c69c7023816689b6a31655c5913
scottwegley <scottwegley@gmail.com>
2023-01-25 23:59:51 -0800

    started work on unique Collection to replace RandomHashSet

---
+++ src/utils/CustomHashSet.java
@@ -0,0 +1,34 @@
+package src.utils;
+
+public class CustomHashSet<T> {
+    private T[] array = (T[]) new Object[1];
+
+    private int size = 0;
+    public void add(T element){
+        array[size] = element;
+        size++;
+        if(size == array.length){
+            T[] biggerArray = (T[]) new Object[size * 2 + 1];
+            for (int i = 0; i < array.length; i++) {
+                biggerArray[i] = array[i]; 
+            }
+            array = biggerArray;
+        }
+    }
+
+    public void remove(int index){
+        for(int i = index; i < size -1; i++){
+            array[i] = array[i + 1];
+        }
+        size--;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public T get(int index){
+        return array[index];
+    }
+
+}
