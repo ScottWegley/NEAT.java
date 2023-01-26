d1597baab1f734c028c9f5a04208ed1d0bc62108
scottwegley <scottwegley@gmail.com>
2023-01-26 10:12:28 -0800

    basic nodegene

--- src/genome/NodeGene.java
+++ src/genome/NodeGene.java
@@ -1,5 +1,35 @@
 package src.genome;
 
-public class NodeGene {
-    
+public class NodeGene extends Gene{
+
+    private double x,y;
+
+    public NodeGene(int innovationNumber) {
+        super(innovationNumber);
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public boolean equals(Object o){
+        if(!(o instanceof NodeGene)) return false;
+        return innovationNumber == ((NodeGene) o).getInnovationNumber();
+    }
+
+    public int hashCode(){
+        return innovationNumber;
+    }
 }
