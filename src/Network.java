bb7186c4ecde7c9d5e2b4ad1f830cf2a8b6c4b5b
Scott Wegley <scbrwegley@protonmail.ch>
2023-01-26 08:30:46 -0800

    formatting

--- src/Network.java
+++ src/Network.java
@@ -6,19 +6,19 @@ import java.util.HashMap;
 import src.NodeGene.TYPE;
 
 public class Network {
-    
-    public static final int MAX_NODES = (int)Math.pow(2,10);
 
-    private HashMap<ConnectionGene, ConnectionGene> connectionDictionary  = new HashMap<>();
+    public static final int MAX_NODES = (int) Math.pow(2, 10);
+
+    private HashMap<ConnectionGene, ConnectionGene> connectionDictionary = new HashMap<>();
     private ArrayList<NodeGene> nodeDictionary = new ArrayList<>();
 
     private int inputNodes, outputNodes, instanceLimit, instances;
 
-    public Network(int inputNodes, int outputNodes, int instanceLimit){
+    public Network(int inputNodes, int outputNodes, int instanceLimit) {
         this.reset(inputNodes, outputNodes, instanceLimit);
     }
 
-    public void reset(int inputNodes, int outputNodes, int instanceLimit){
+    public void reset(int inputNodes, int outputNodes, int instanceLimit) {
         this.inputNodes = inputNodes;
         this.outputNodes = outputNodes;
         this.instanceLimit = instanceLimit;
@@ -26,15 +26,19 @@ public class Network {
         connectionDictionary.clear();
         nodeDictionary.clear();
 
-        for (int i = 0; i < inputNodes; i++) {
+        for (int i = 0; i < this.inputNodes; i++) {
             NodeGene nodeGene = generateNode();
             nodeGene.setType(TYPE.INPUT_NODE);
         }
 
-        for (int i = 0; i < outputNodes; i++) {
+        for (int i = 0; i < this.outputNodes; i++) {
             NodeGene nodeGene = generateNode();
             nodeGene.setType(TYPE.OUTPUT_NODE);
         }
+    }
+
+    public HashMap<ConnectionGene, ConnectionGene> getConnectionDictionary() {
+        return connectionDictionary;
     }
 
     public NodeGene generateNode() {
@@ -44,7 +48,8 @@ public class Network {
     }
 
     public NodeGene getNode(int index) {
-        if(index >= 0 && index < nodeDictionary.size()) return nodeDictionary.get(index);
+        if (index >= 0 && index < nodeDictionary.size())
+            return nodeDictionary.get(index);
         return generateNode();
     }
 
