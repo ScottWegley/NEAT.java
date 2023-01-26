905e5390b43f00d5cba4dd05a8ff7e0f962503b8
scottwegley <scottwegley@gmail.com>
2023-01-26 00:49:32 -0800

    getters, setters, constructors for NodeGene

--- src/NodeGene.java
+++ src/NodeGene.java
@@ -1,12 +1,30 @@
 package src;
 public class NodeGene {
 
-    private int id;
+    private int innovationNumber;
     private TYPE type;
+
+    public TYPE getType() {
+        return type;
+    }
+
+    public void setType(TYPE type) {
+        this.type = type;
+    }
+
+    public NodeGene(int innovationNumber, TYPE type) {
+        this(innovationNumber);
+        this.type = type;
+    }
+
+    public NodeGene(int innovationNumber){
+        this.innovationNumber = innovationNumber;
+    }
 
     public enum TYPE {
         INPUT_NODE,
         HIDDEN_NODE,
         OUTPUT_NODE
     }
+
 }
