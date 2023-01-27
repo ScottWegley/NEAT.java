e0937b58e1688c5d112e8723bfc0499ad68ef633
scottwegley <scottwegley@gmail.com>
2023-01-26 19:35:25 -0800

    formatting for customhashset

--- src/utils/CustomHashSet.java
+++ src/utils/CustomHashSet.java
@@ -4,30 +4,34 @@ public class CustomHashSet<T> {
     private T[] array = (T[]) new Object[1];
 
     private int size = 0;
-    public void add(T element){
-        if(contains(element)) return;
+
+    public void add(T element) {
+        if (contains(element)) {
+            return;
+        }
         array[size] = element;
         size++;
-        if(size == array.length){
+        if (size == array.length) {
             T[] biggerArray = (T[]) new Object[size * 2 + 1];
             for (int i = 0; i < array.length; i++) {
-                biggerArray[i] = array[i]; 
+                biggerArray[i] = array[i];
             }
             array = biggerArray;
         }
     }
 
-    public void remove(int index){
-        if(!validIndex(index)) return;
-        for(int i = index; i < size -1; i++){
+    public void remove(int index) {
+        if (!validIndex(index))
+            return;
+        for (int i = index; i < size - 1; i++) {
             array[i] = array[i + 1];
         }
         size--;
     }
 
-    public void remove(T element){
-        for (int i = 0; i < size; i++){
-            if(element.equals(array[i])){
+    public void remove(T element) {
+        for (int i = 0; i < size; i++) {
+            if (element.equals(array[i])) {
                 remove(i);
                 return;
             }
@@ -38,18 +42,19 @@ public class CustomHashSet<T> {
         return size;
     }
 
-    public T getElement(int index){
+    public T getElement(int index) {
         return validIndex(index) ? array[index] : null;
     }
 
-    public T getRandomElement(){
-        if(size == 0) return null;
-        return array[(int)Math.random() *size];
+    public T getRandomElement() {
+        if (size == 0)
+            return null;
+        return array[(int) Math.random() * size];
     }
 
-    public int getIndex(T element){
+    public int getIndex(T element) {
         for (int i = 0; i < size; i++) {
-            if(element.equals(array[i])){
+            if (element.equals(array[i])) {
                 return i;
             }
         }
@@ -65,11 +70,11 @@ public class CustomHashSet<T> {
         return false;
     }
 
-    public void clear(){
+    public void clear() {
         array = (T[]) new Object[1];
     }
 
-    private boolean validIndex(int index){
+    private boolean validIndex(int index) {
         return (index >= 0 && index < size);
     }
 }
